ecb9b80f3c3d35cd4eb5d4525bce0955c185367e
Isaac Mehlhaff <isaac.mehlhaff@gmail.com>
2025-07-27 12:59:10 -0500

    Update citation, roxygen version

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# PolarCAP 1.1.2
+
+* Updates citation information
+
 # PolarCAP 1.0.1
 
 * Initial CRAN submission.
--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("\nTo cite in publications and working papers, please use:\n")
-  packageStartupMessage(" Mehlhaff, Isaac D. Mass Polarization across Time and Space. Book manuscript. July 2024.\n")
+  packageStartupMessage(" Mehlhaff, Isaac D. Mass Polarization across Time and Space. New York: Cambridge University Press (2025).\n")
 }
